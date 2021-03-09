c0723a7476102be90ab2cd6f71f3cf6ceb7b25a6
Reece Russell <me@reece-russell.co.uk>
2021-03-09 18:51:39 +0000

    Added a service collection extension to register services

---
+++ TxCommand.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Data;
+using TxCommand.Abstractions;
+using Xunit;
+
+namespace TxCommand.Tests
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddTxCommand_SetsUpServices_ReturnsInitialCollectionWithServices()
+        {
+            var services = new ServiceCollection()
+                .AddScoped(_ => Mock.Of<IDbConnection>());
+
+            services.AddTxCommand();
+
+            var provider = services.BuildServiceProvider();
+
+            Assert.IsType<TxCommandExecutorFactory>(provider.GetRequiredService<ITxCommandExecutorFactory>());
+            Assert.IsType<TxCommandExecutor>(provider.GetRequiredService<ITxCommandExecutor>());
+        }
+
+        [Fact]
+        public void AddTxCommand_RetrievingITxCommandExecutorFactoryInSameScope_ReturnsNewInstances()
+        {
+            var services = new ServiceCollection()
+                .AddScoped(_ => Mock.Of<IDbConnection>());
+
+            services.AddTxCommand();
+
+            var provider = services.BuildServiceProvider()
+                .CreateScope()
+                .ServiceProvider;
+
+            var factory1 = provider.GetRequiredService<ITxCommandExecutorFactory>();
+            var factory2 = provider.GetRequiredService<ITxCommandExecutorFactory>();
+
+            // Factories are transient.
+            Assert.NotEqual(factory1, factory2);
+        }
+
+        [Fact]
+        public void AddTxCommand_RetrievingITxCommandExecutorInSameScope_ReturnsNewInstances()
+        {
+            var services = new ServiceCollection()
+                .AddScoped(_ => Mock.Of<IDbConnection>());
+
+            services.AddTxCommand();
+
+            var provider = services.BuildServiceProvider()
+                .CreateScope()
+                .ServiceProvider;
+
+            var executor1 = provider.GetRequiredService<ITxCommandExecutor>();
+            var executor2 = provider.GetRequiredService<ITxCommandExecutor>();
+
+            // Executors are transient.
+            Assert.NotEqual(executor1, executor2);
+        }
+
+        [Fact]
+        public void ResolveCommandExecutorFactory_WithNoIDbConnectionService_Throws()
+        {
+            var services = new ServiceCollection();
+
+            services.AddTxCommand();
+
+            var provider = services.BuildServiceProvider();
+
+            Assert.Throws<InvalidOperationException>(() => provider.GetRequiredService<ITxCommandExecutorFactory>());
+        }
+    }
+}
---
+++ TxCommand/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TxCommand.Abstractions;
+
+namespace TxCommand
+{
+    /// <summary>
+    /// ServiceCollectionExtensions provides a collection of extension methods for <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Registers the required services for using the TxCommand interfaces.
+        /// </summary>
+        /// <param name="services">An instance of <see cref="IServiceCollection"/>.</param>
+        /// <returns><paramref name="services"/> with the TxCommand services.</returns>
+        public static IServiceCollection AddTxCommand(this IServiceCollection services)
+        {
+            return services.AddTransient<ITxCommandExecutorFactory, TxCommandExecutorFactory>()
+                .AddTransient<ITxCommandExecutor>(provider =>
+                {
+                    var factory = provider.GetRequiredService<ITxCommandExecutorFactory>();
+
+                    return factory.Create();
+                });
+        }
+    }
+}

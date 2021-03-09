4b73b98330319a8bc124edbf0c6c7c712ea30cfd
Reece Russell <me@reece-russell.co.uk>
2021-03-09 18:35:22 +0000

    Implmented the command executor factory

---
+++ TxCommand.Tests/TxCommandExecutorFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using System.Data;
+using Xunit;
+
+namespace TxCommand.Tests
+{
+    public class TxCommandExecutorFactoryTests
+    {
+        [Fact]
+        public void Create_WithIDBConnectionDependency_ReturnsNewCommandExecutor()
+        {
+            var connection = new Mock<IDbConnection>();
+            connection.SetupGet(x => x.State).Returns(ConnectionState.Open).Verifiable();
+
+            var factory = new TxCommandExecutorFactory(connection.Object);
+
+            _ = factory.Create();
+
+            // Proves connection was passed through to the new command executor.
+            connection.Verify(x => x.State, Times.Once);
+        }
+    }
+}

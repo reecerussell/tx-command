2a85a8216226b0f25f57687980eecbbcf74cfad4
Reece Russell <me@reece-russell.co.uk>
2021-03-09 18:27:48 +0000

    Added an executor interface, used to execute the ITxCommand interface

---
+++ TxCommand.Abstractions/ITxCommandExecutor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace TxCommand.Abstractions
+{
+    /// <summary>
+    /// Used to execute <see cref="ITxCommand"/>s, providing them with an open <see cref="IDbTransaction"/>.
+    /// <see cref="ITxCommandExecutor"/> acts as a transaction, therefore should only used on a per-scope basis,
+    /// and only for a specific collection of <see cref="ITxCommand"/>s with a specific area of concern.
+    /// </summary>
+    public interface ITxCommandExecutor : IDisposable
+    {
+        /// <summary>
+        /// Commits the underlying <see cref="IDbTransaction"/>.
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// Rolls back the underlying <see cref="IDbTransaction"/>.
+        /// </summary>
+        void Rollback();
+
+        /// <summary>
+        /// Executes the given <see cref="ITxCommand"/>, <paramref name="command"/>, within the current transaction.
+        /// </summary>
+        /// <param name="command">The command to execute.</param>
+        /// <returns></returns>
+        Task ExecuteAsync(ITxCommand command);
+
+        /// <summary>
+        /// Executes the given <see cref="ITxCommand"/>, <paramref name="command"/>, within the current transaction.
+        /// </summary>
+        /// <param name="command">The command to execute.</param>
+        /// <returns>The command's output, <typeparamref name="TResult"/>.</returns>
+        Task<TResult> ExecuteAsync<TResult>(ITxCommand<TResult> command);
+    }
+}

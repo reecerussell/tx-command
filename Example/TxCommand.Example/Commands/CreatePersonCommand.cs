8b81d657f3562091136d00cf550993429a985e97
Reece Russell <me@reece-russell.co.uk>
2021-03-10 12:39:19 +0000

    Updated example command with validation

--- Example/TxCommand.Example/Commands/CreatePersonCommand.cs
+++ Example/TxCommand.Example/Commands/CreatePersonCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using System;
 using System.Data;
 using System.Threading.Tasks;
 using TxCommand.Abstractions;
@@ -19,6 +20,14 @@ namespace TxCommand.Example.Commands
             const string query = "INSERT INTO [People] ([Name]) VALUES (@Name); SELECT SCOPE_IDENTITY();";
 
             return await transaction.Connection.ExecuteScalarAsync<int>(query, new {Name}, transaction);
+        }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                throw new ArgumentException("Name cannot be empty", nameof(Name));
+            }
         }
     }
 }

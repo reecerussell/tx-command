2dd46c01b0cdfe81110d62721e6a279ab3c21654
Reece Russell <me@reece-russell.co.uk>
2021-12-09 10:37:17 +0000

    Added an overload to create to pass custom IDbConnection

--- Sql/TxCommand.Sql/SessionFactory.cs
+++ Sql/TxCommand.Sql/SessionFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using Microsoft.Extensions.DependencyInjection;
 using TxCommand.Abstractions;
 
 namespace TxCommand
@@ -10,6 +12,26 @@ namespace TxCommand
     {
         public SessionFactory(IServiceProvider services) : base(services)
         {
+        }
+
+        /// <summary>
+        /// Create returns a new session with the given <paramref name="connection"/>.
+        /// This is an overload used to provide <see cref="IDbConnection"/> that is
+        /// different to the one configured in dependency injection.
+        /// </summary>
+        /// <param name="connection">The connection to use.</param>
+        /// <returns>A new instance of <see cref="ISession"/>.</returns>
+        public ISession Create(IDbConnection connection)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+
+            var options = Services.GetRequiredService<SqlOptions>();
+            var provider = new TransactionProvider(connection, options);
+
+            return new Session(provider);
         }
     }
 }

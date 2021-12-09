08c25d979bd70fe7220c16c15f416642f93cc4f0
Reece Russell <me@reece-russell.co.uk>
2021-12-09 14:18:32 +0000

    Added missing method to interface

--- Sql/TxCommand.Sql.Abstractions/ISessionFactory.cs
+++ Sql/TxCommand.Sql.Abstractions/ISessionFactory.cs
@@ -1,9 +1,19 @@
-﻿namespace TxCommand.Abstractions
+﻿using System.Data;
+
+namespace TxCommand.Abstractions
 {
     /// <summary>
     /// Used to create new instance of <see cref="ISession"/> for Sql.
     /// </summary>
     public interface ISessionFactory : ISessionFactory<ISession>
     {
+        /// <summary>
+        /// Create returns a new session with the given <paramref name="connection"/>.
+        /// This is an overload used to provide <see cref="IDbConnection"/> that is
+        /// different to the one configured in dependency injection.
+        /// </summary>
+        /// <param name="connection">The connection to use.</param>
+        /// <returns>A new instance of <see cref="ISession"/>.</returns>
+        ISession Create(IDbConnection connection);
     }
 }
